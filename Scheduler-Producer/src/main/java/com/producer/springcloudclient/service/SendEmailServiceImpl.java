b45d403e0e97e0213c7072ffee340099b530b5dd
FEIJI <bhd_xiaofeiji@163.com>
2022-06-27 16:09:13 +0800

    加入线程池，实现邮件延迟发送

--- Scheduler-Producer/src/main/java/com/producer/springcloudclient/service/SendEmailServiceImpl.java
+++ Scheduler-Producer/src/main/java/com/producer/springcloudclient/service/SendEmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.producer.springcloudclient.service;
 
+import com.producer.springcloudclient.config.thread.ThreadPoolGroup;
 import com.producer.springcloudclient.manager.EmailManager;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 @Slf4j
 @Service
@@ -32,10 +35,26 @@ public class SendEmailServiceImpl implements SendEmailService{
             mimeMessageHelper.setTo(toPeopleName);  // 设置收件人
             mimeMessageHelper.setSubject(mailTitle);    // 设置主题/标题
             mimeMessageHelper.setText(content,true);     // 设置内容.true:支持html格式
-            sender.send(mimeMessageHelper.getMimeMessage());    // 发送邮件
+            //设置邮件发送状态为发送中
+            ThreadPoolGroup.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        sender.send(mimeMessageHelper.getMimeMessage());    // 发送邮件
+                    } catch (MailException e) {
+                        e.printStackTrace();
+                        logger.error(String.format("邮件发送 失败 e:{%s}", e));
+                        //设置邮件发送失败
+                        return;
+                    }
+
+                }
+            });
+            //设置邮件发送状态为发送成功
             logger.info("邮件发送成功...");
             return true;
         } catch (MessagingException e) {
+            //设置邮件发送状态为发送失败
             logger.error(String.format("邮件发送 失败 e:{%s}", e));
             return false;
         }
